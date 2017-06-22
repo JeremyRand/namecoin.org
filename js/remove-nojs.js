374d2db58a9a7a512577766973852d15aa9d8804
JeremyRand <biolizard89@gmail.com>
2017-06-22 06:28:21 +0000

    Theme: remove inline Javascript.

--- js/remove-nojs.js
+++ js/remove-nojs.js
@@ -1,1 +1,15 @@
+// remove nojs
 document.documentElement.classList.remove("nojs");
+
+// random tagline
+var taglines = [
+    '<strong>In information</strong><br />we <strong>trust</strong>.',
+    'A <strong>trust anchor</strong><br />for the Internet.',
+    '<strong>Against<br />censorship.</strong>',
+    '<strong>Supporting<br />free speech.</strong>',
+    'Decentralized<br /><strong>secure</strong> names.',
+    '<b>Decentralize</b><br>all the things!',
+    '<b>Freedom</b><br>of information.'
+    ];
+var rand = taglines[Math.floor(Math.random() * taglines.length)];
+document.getElementById("tagline").innerHTML = rand;
