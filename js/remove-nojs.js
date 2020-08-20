ddd5fbb7cfa52e4d53c3386dafe545d74d9afa2c
Jeremy Rand <jeremyrand@airmail.cc>
2020-08-20 18:05:56 +0000

    Header: avoid innerHTML
    innerHTML is poor security practice.

--- js/remove-nojs.js
+++ js/remove-nojs.js
@@ -2,14 +2,7 @@
 document.documentElement.classList.remove("nojs");
 
 // random tagline
-var taglines = [
-    '<strong>In information</strong><br />we <strong>trust</strong>.',
-    'A <strong>trust anchor</strong><br />for the Internet.',
-    '<strong>Against<br />censorship.</strong>',
-    '<strong>Supporting<br />free speech.</strong>',
-    'Decentralized<br /><strong>secure</strong> names.',
-    '<strong>Decentralize</strong><br>all the things!',
-    '<strong>Freedom</strong><br>of information.'
-    ];
-var rand = taglines[Math.floor(Math.random() * taglines.length)];
-document.getElementById("tagline").innerHTML = rand;
+var taglines = document.getElementById("tagline").getElementsByClassName("candidate-tagline");
+var rand_tagline = taglines[Math.floor(Math.random() * taglines.length)];
+
+rand_tagline.removeAttribute("hidden");
