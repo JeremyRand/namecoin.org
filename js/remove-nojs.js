396d6716a10fe0176a3c3ab4f7cc3af18e35010d
Jeremy Rand <jeremyrand@airmail.cc>
2021-03-16 12:34:43 +0000

    Remove noscript content on DOM ready event
    Should fix flickering on page load.

--- js/remove-nojs.js
+++ js/remove-nojs.js
@@ -1,10 +1,12 @@
-// remove nojs
-document.documentElement.classList.remove("nojs");
+$(function () {
+	// remove nojs
+	document.documentElement.classList.remove("nojs");
 
-// random tagline
-var taglines = document.getElementById("tagline").getElementsByClassName("candidate-tagline");
-var rand_tagline = taglines[Math.floor(Math.random() * taglines.length)];
-var default_tagline = document.getElementById("default-tagline");
+	// random tagline
+	var taglines = document.getElementById("tagline").getElementsByClassName("candidate-tagline");
+	var rand_tagline = taglines[Math.floor(Math.random() * taglines.length)];
+	var default_tagline = document.getElementById("default-tagline");
 
-default_tagline.setAttribute("hidden", "");
-rand_tagline.removeAttribute("hidden");
+	default_tagline.setAttribute("hidden", "");
+	rand_tagline.removeAttribute("hidden");
+});
