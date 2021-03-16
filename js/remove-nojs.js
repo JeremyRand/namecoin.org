8e69387769a0ca9b6068f71416189a6a3d25ecf6
Jeremy Rand <jeremyrand@airmail.cc>
2021-03-16 11:56:14 +0000

    Avoid requiring "noscript" permission for tagline
    Should fix the tagline in Tor Browser Safest mode.

--- js/remove-nojs.js
+++ js/remove-nojs.js
@@ -4,5 +4,7 @@ document.documentElement.classList.remove("nojs");
 // random tagline
 var taglines = document.getElementById("tagline").getElementsByClassName("candidate-tagline");
 var rand_tagline = taglines[Math.floor(Math.random() * taglines.length)];
+var default_tagline = document.getElementById("default-tagline");
 
+default_tagline.setAttribute("hidden", "");
 rand_tagline.removeAttribute("hidden");
